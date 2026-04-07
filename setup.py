d48ec762bcefb504acb0debdb99a0abd766401ee
Pablo Laciana <placiana@dc.uba.ar>
2026-04-07 11:48:51 -0300

    fix setuptools

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
-from distutils.core import setup
+from setuptools import setup, find_packages
 
 setup(
     name='django-enhanced-cbv',
     version='0.1.dev',
     author='Ivan Raskovsky (rasca)',
     author_email='raskovsky@gmail.com',
-    packages=['enhanced_cbv',],
+    packages=find_packages(),
     license='BSD',
     description='generic class based views with enhanced functionallity',
     long_description=open('README.txt').read(),
